d361e3df38a070be8a9d0cba8903507ad7d81d62
Alexandre Morgado <alexandremorgado@gmail.com>
2020-09-14 18:24:37 -0300

    Adding localization support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftDate",
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
